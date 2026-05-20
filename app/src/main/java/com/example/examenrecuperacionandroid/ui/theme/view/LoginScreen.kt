488ed908174a659eddc9cd14061b1b4096d29047
Dalton Vargas <dvarper1312@g.educaand.es>
2026-05-20 17:22:13 +0200

    tercer commit

--- app/src/main/java/com/example/examenrecuperacionandroid/ui/theme/view/LoginScreen.kt
+++ app/src/main/java/com/example/examenrecuperacionandroid/ui/theme/view/LoginScreen.kt
@@ -3,16 +3,40 @@ package com.example.examenrecuperacionandroid.ui.theme.view
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.examenrecuperacionandroid.R
 
 @Composable
 fun LoginScreen() {
+    var email by rememberSaveable() {
+        mutableStateOf("")
+    }
+    var password by rememberSaveable() {
+        mutableStateOf("")
+    }
+    var mostrarPassword by rememberSaveable() {
+        mutableStateOf(false)
+    }
+    var esIncorrecta by rememberSaveable() {
+        mutableStateOf(false)
+    }
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -20,9 +44,63 @@ fun LoginScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(id = R.drawable.logo)
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
         )
+        Spacer(modifier = Modifier.height(30.dp))
 
+        TextField(
+            value = email,
+            onValueChange = {
+                email=it
+            },
+            label ={
+                Text("Email")
+            }
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(
+            value= password,
+            onValueChange = {
+                password=it
+            },
+            label = {
+                Text("Contraseña")
+            },
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(
+            onClick = {
+
+            },
+           
+        ) {
+            Text("Login")
+        }
+        if (esIncorrecta){
+            AlertDialog(
+                onDismissRequest = {
+                    esIncorrecta=false
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            esIncorrecta=false
+                        }
+                    ) {
+                        Text("Aceptar")
+                    }
+                },
+                title = {
+                    Text("Login")
+                },
+                text = {
+                    Text("Usuario o contraseña incorrectos")
+                }
+            )
+        }
     }
 }
 @Preview(showBackground = true)
